e61009b329360b6a16a51a08e5df72e979b3317b
Martin Chapman <contact@martinchapman.co.uk>
2024-05-24 14:55:47 +0100

    Update made by Phenoflow

---
+++ python/lymphocytic-cancer---primary.py
@@ -0,0 +1,25 @@
+# Evangelos Kontopantelis, David A Springate, David Reeves, Darren M. Aschroff, Martin Rutter, Iain Buchan, Tim Doran, Matthias Pierce, Darren M. Ashcroft, 2024.
+
+import sys, csv, re
+
+codes = [{"code":"B621800","system":"readv2"},{"code":"B614800","system":"readv2"},{"code":"B61z800","system":"readv2"},{"code":"B620500","system":"readv2"},{"code":"B601500","system":"readv2"},{"code":"B62y500","system":"readv2"},{"code":"B61z500","system":"readv2"},{"code":"B62y800","system":"readv2"},{"code":"B620800","system":"readv2"},{"code":"B6z0.00","system":"readv2"},{"code":"B602500","system":"readv2"},{"code":"B64y100","system":"readv2"},{"code":"B621500","system":"readv2"}];
+REQUIRED_CODES = 1;
+with open(sys.argv[1], 'r') as file_in, open('cancer-potential-cases.csv', 'w', newline='') as file_out:
+    csv_reader = csv.DictReader(file_in)
+    csv_writer = csv.DictWriter(file_out, csv_reader.fieldnames + ["lymphocytic-cancer---primary-identified"])
+    csv_writer.writeheader();
+    codes_identified = 0;
+    for row in csv_reader:
+        newRow = row.copy();
+        for cell in row:
+            # Iterate cell lists (e.g. codes)
+            for item in re.findall(r'\(([^,]*)\,', row[cell]):
+                if(item in list(map(lambda code: code['code'], codes))): codes_identified+=1;
+                if(codes_identified>=REQUIRED_CODES):
+                    newRow["lymphocytic-cancer---primary-identified"] = "CASE";
+                    break;
+            if(codes_identified>=REQUIRED_CODES): break;
+        if(codes_identified<REQUIRED_CODES):
+            newRow["lymphocytic-cancer---primary-identified"] = "UNK";
+        codes_identified=0;
+        csv_writer.writerow(newRow)
